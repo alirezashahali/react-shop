d319ff00ce3ad8f2503540e9fb0d27568932bdb8
Alireza <shahali.alireza.92@gmail.com>
2020-06-05 00:42:44 +0430

    using reselect and all

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button'
 
 import './cart-dropdown.scss'
+import CartItem from '../cart-item/cart-item'
 
-const CartDropdown = () => (
+import { selectCartItems } from '../../redux/cart/cart.selectors'
+
+const CartDropdown = ({cartItems, cartCount}) => (
     <div className="cart-dropdown" >
-        <div className="cart-items">
+        <div className="cart-items"> 
+            {cartItems.map((cartItem) =>(
+              <CartItem key={cartItem.id} item = {cartItem}></CartItem>
+            ))}
           <CustomButton>GO TO CHECKOUT </CustomButton>
         </div>
     </div>
 )
 
-export default CartDropdown
+const mapStateToProps = (state) => (
+  { cartItems: selectCartItems(state),
+    cartCount: selectCartItems(state).reduce((acc, cartItem)=> (acc + cartItem.quantity), 0)
+  }
+)
+
+export default connect(mapStateToProps)(CartDropdown)
