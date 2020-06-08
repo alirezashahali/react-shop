f7c4dc07863dfff47c328d19a59c8cc4d2bfe8e4
Alireza <shahali.alireza.92@gmail.com>
2020-06-08 17:51:22 +0430

    ready for heroku

--- src/pages/check-out/check-out.jsx
+++ src/pages/check-out/check-out.jsx
@@ -44,6 +44,13 @@ const CheckOut = ( { cartItems, overAllPrice, toggleCartHidden, removeItem } ) =
         }
         <div className='total'>TOTAL: ${overAllPrice}</div>
 
+        <div className='aclamation'>
+            <div>"Please use the following test credit card for payment"</div>
+            <div>
+                4242 4242 4242 4242-Exp:01/20-CVV:123
+            </div>
+        </div>
+
         <div className="margin-adder">
             <StripeCheckout price={overAllPrice}/>
         </div>
